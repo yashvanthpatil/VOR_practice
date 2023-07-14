938e4c6502e649c8dbc6e9d79c6ff15d1b8ee248
yashvanth patil <yashvanthpatil143@gmail.com>
2023-07-15 01:06:56 +0530

    Added vor_report and Test_vor_report

---
+++ VOR_project/src/test/java/Test_VOR_pages/Test_vor_report.java
@@ -0,0 +1,50 @@
+package Test_VOR_pages;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+import VOR_Base.VOR_base_class1;
+import VOR_Pages.Homepage_vor;
+import VOR_Pages.Login_page_vor;
+import VOR_Pages.Vor_report;
+
+public class Test_vor_report extends VOR_base_class1 {
+			
+	@Test
+	public void vor_report_test() throws InterruptedException {
+	Login_page_vor lvor = new Login_page_vor(driver);
+    lvor.click_login_link();
+    Set<String> windows = driver.getWindowHandles();
+    Iterator<String> it = windows.iterator();
+    String parent = it.next();
+    String child = it.next();
+    driver.switchTo().window(child);
+    Thread.sleep(2000);
+    lvor.enter_email();
+    lvor.click_button();
+    Thread.sleep(2000);
+    lvor.enter_password();
+    lvor.click_button();
+    Thread.sleep(2000);
+    lvor.click_button();
+    Thread.sleep(2000);
+    System.out.println("1 time execution completed");
+    driver.switchTo().window(parent);
+    Thread.sleep(10000);
+	Homepage_vor hp = new Homepage_vor(driver);
+	hp.movetomenu();
+	System.out.println("expanded the nav bar");
+	waitfor(8000);
+	hp.clickvorreport();
+	Thread.sleep(2000);
+	hp.movetomenu();
+	Vor_report vr=new Vor_report(driver);
+	vr.Add_new();
+	vr.logg_date();
+	 Thread.sleep(3000);
+	vr.select_cust();
+	vr.select_cust_list();
+}
+}
--- VOR_project/src/test/java/VOR_Pages/Vor_report.java
+++ VOR_project/src/test/java/VOR_Pages/Vor_report.java
@@ -1,5 +1,101 @@
 package VOR_Pages;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Vor_report {
 		
+	@FindBy(xpath = "//button[.=\" Add new \"]")
+	private WebElement addnew_btn;
+	
+	@FindBy(xpath = "//input[@formcontrolname=\"LogdateAdd\"]")
+	private WebElement Loggdateadd;
+	
+	@FindBy(xpath = "//ng-select[@role=\"listbox\"]/div[@class=\"ng-select-container\"]//div[2]/input")
+	private WebElement Customername;
+	
+	@FindBy(xpath = "//span[@class=\"ng-arrow-wrapper\"]")
+	private WebElement cust_name_arrow_btn;
+	//      
+	@FindBy(xpath = "//div[@class=\"ng-dropdown-panel-items scroll-host\"]/div[2]/div")
+	private List<WebElement> customerList;
+	
+//	@FindBy(xpath = "//div[@class=\"modal-content\"]//div[.=\"Select Customer\"]/div/div[1]")
+//	private WebElement enter_custName;
+	
+	@FindBy(xpath = "//div[@class=\"modal-content\"]//div[.=\"Select Customer\"]//input")
+	private WebElement enter_custName;
+	
+	
+	public Vor_report(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void Add_new() {
+		addnew_btn.click();
+	}
+	
+	
+	public void logg_date() {
+	Loggdateadd.click();
+	Loggdateadd.clear();
+	Loggdateadd.click();
+	        // Get the current date
+	        LocalDate currentDate = LocalDate.now();
+
+	        // Format the date according to your desired format
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	        String formattedDate = currentDate.format(formatter);
+
+	        System.out.println("Current date: " + formattedDate);
+	        
+	        Loggdateadd.sendKeys(formattedDate);
+
+	        // Assuming you want to use the current date in your Selenium code
+	        // You can pass the 'formattedDate' value to your Selenium code
+	    }
+	
+	public void select_cust() {
+		//Customername.click();
+		cust_name_arrow_btn.click();
+		//enter_custName.click();
+	}
+	
+	public void select_cust_list() {
+		int cust_count = customerList.size();
+		System.out.println(cust_count);
+		for (int i = 0; i < cust_count; i++) {
+	        WebElement custElement = customerList.get(i);
+	        String name = custElement.getText();
+	        System.out.println("Name at index " + i + ": " + name);
+	       
+	       
+		 if(name.contains("kumar")) {
+			 custElement.click();
+			 break;
+			 
+		 	}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
